a7f07cc51f99bf5bbc6032f0820fc712faa1b7e0
Lindys Arely Martinez Herrera <herrerarely2003@gmail.com>
2022-08-15 10:45:09 -0600

    Ejercicio 2

---
+++ Ejecicio 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ejecicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero:");
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Elijió la opción {0} ", option);
+                    break;
+                case 2:
+                    Console.WriteLine("Elijió la opción {0} ", option);
+                    break;
+                case 3:
+                    Console.WriteLine("Elijió la opción {0} ", option);
+                    break;
+                case 4:
+                    Console.WriteLine("Elijió la opción {0} ", option);
+                    break;
+                case 5:
+                    Console.WriteLine("Elijió la opción {0} ", option);
+                    break;
+                default:
+                    Console.WriteLine("Esta opción no esta disponble");
+                    break;
+
+
+
+
+            }
+        }
+    }
+}
