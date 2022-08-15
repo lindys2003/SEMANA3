c9c695cf249edb0b30be045630884518ee2fbbe0
Lindys Arely Martinez Herrera <herrerarely2003@gmail.com>
2022-08-15 10:03:13 -0600

    Agregar archivos de proyecto.

---
+++ SEMANA3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SEMANA3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nota promedio: ");
+            double nota = double.Parse(Console.ReadLine());
+
+            if (nota >= 0 && nota <= 10) 
+            {
+                if (nota >= 6) 
+                {
+                    Console.WriteLine("Aprovado");
+                } 
+                else if (nota >= 5.0)
+                {
+                    Console.WriteLine("Reposicion");
+                }
+                else 
+                {
+                    Console.WriteLine("Reprobado");
+                }
+            }
+            else
+            {
+                Console.WriteLine("nota invalida");
+
+            }
+        }
+    }
+}
