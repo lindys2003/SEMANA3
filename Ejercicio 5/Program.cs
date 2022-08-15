f0803cf3dcc88b021ad7c2cac535a68f9eacf482
Lindys Arely Martinez Herrera <herrerarely2003@gmail.com>
2022-08-15 12:11:04 -0600

    ejercicio 5

---
+++ Ejercicio 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            string linea;
+            do
+            {
+                Console.WriteLine("Ingrese uun valor entre 0 y 999 (0 finalizada):");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                if (valor >= 100)
+                {
+                    Console.WriteLine("Tiene 3 dígitos.");
+                }
+                else if (valor >= 10)
+                {
+                    Console.WriteLine("Tiene 2 dígitos.");
+                }
+                else
+                {
+                    Console.WriteLine("Tiene 1 dígito.");
+                }
+
+            } while (valor != 0);
+        }
+    }
+}
