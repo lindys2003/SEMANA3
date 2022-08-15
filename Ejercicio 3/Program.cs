482cbd72b5f3997111561c6328165c741eaf95a8
Lindys Arely Martinez Herrera <herrerarely2003@gmail.com>
2022-08-15 11:07:20 -0600

    Ejercicio 3

---
+++ Ejercicio 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el numero a evaluar:");
+            int multiplo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el numero del limite:");
+            int limite = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= limite; i++) 
+
+            {
+                if (i %multiplo == 0)
+                {
+                    Console.Write(i + " ");
+
+                }
+            }
+        }
+    }
+}
