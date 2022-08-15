0d44484d44aea24af2e125e1b41764cf5da32ecc
Lindys Arely Martinez Herrera <herrerarely2003@gmail.com>
2022-08-15 11:26:28 -0600

    ejercicio 4

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+            int cont = 0;
+            Console.WriteLine("Ingrese el numero:");
+            int limite = int.Parse(Console.ReadLine());
+            while (condition)
+            {
+                if (cont == limite)
+                    condition = false;
+                cont++;
+                Console.WriteLine(cont);
+            }
+         }
+     }
+}
